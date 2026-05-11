a92bd8e6acce5abdde28986b5cb68baf906e37aa
AnEye <shakhar004@gmail.com>
2026-05-11 13:52:07 +0530

    feat : auth system added and tested without jwt

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/connectDB.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "RentEase API Running",
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
